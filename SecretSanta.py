6f495b6dc74eecafc07c2558892cd326209ddad0
EmuWarfare <68507214+EmuWarfare@users.noreply.github.com>
2021-11-26 12:58:14 -0500

    Mid Transition
    Transitioning from individual functions to one main class

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,49 +1,5 @@
-
-
-class Graph():
-    def __init__(self, graph):
-        self.graph = graph  # residual graph
-        self. ROW = len(graph)
-        # self.COL = len(gr[0])
- 
-    '''Returns true if there is a path from source 's' to sink 't' in
-    residual graph. Also fills parent[] to store the path '''
- 
-    def BFS(self, s, t, parent):
- 
-        # Mark all the vertices as not visited
-        visited = [False]*(self.ROW)
- 
-        # Create a queue for BFS
-        queue = []
- 
-        # Mark the source node as visited and enqueue it
-        queue.append(s)
-        visited[s] = True
- 
-         # Standard BFS Loop
-        while queue:
- 
-            # Dequeue a vertex from queue and print it
-            u = queue.pop(0)
- 
-            # Get all adjacent vertices of the dequeued vertex u
-            # If a adjacent has not been visited, then mark it
-            # visited and enqueue it
-            for ind, val in enumerate(self.graph[u]):
-                if visited[ind] == False and val > 0:
-                      # If we find a connection to the sink node,
-                    # then there is no point in BFS anymore
-                    # We just have to set its parent and can return true
-                    queue.append(ind)
-                    visited[ind] = True
-                    parent[ind] = u
-                    if ind == t:
-                        return True
- 
-        # We didn't reach sink in BFS starting
-        # from source, so return false
-        return False
+from os import name
+import random
 
 class Person():
     def __init__(self, name) ->None:
@@ -64,17 +20,76 @@ class Person():
     
     def summation(self, importantQ, weight):
         sum = 0
-        for i in range(len(self.multipleChoice)):
+        for i in self.multipleChoice:
             if i in importantQ:
                 sum += weight
             else:
                 sum += 1
-        for i in range(len(self.negMultipleChoice)):
+        for i in self.negMultipleChoice:
             if i in importantQ:
                 sum -= weight
             else:
                 sum -= 1
         self.sum = sum
+
+class SecretSanta():
+    def __init__(self, fileLocal, negativeQ, positiveQ, importantQ):
+        self.fileLocal = fileLocal
+        self.negativeQ = negativeQ
+        self.positiveQ = positiveQ
+        self.importantQ = importantQ
+        self.people = []
+        self.names = []
+    
+    #when similar values, n, is odd then the code returns even number of similar values still
+    def closestValues(list, i, n):
+        l = i - (n//2)
+        r = i + (n//2)
+
+        #if the list is not long enough on the left side, compensate with the right and vice versa
+        while l < 0:
+            r += 1
+            l += 1
+        while r > (len(list) - 1):
+            r -= 1
+            l -= 1
+        close = list[l: i]
+        close.extend(list[i+1: r+1])
+        return close
+    
+    def fileRead(self):
+        #file reading
+        while line:
+            line = line.rstrip()
+            answers = line.split(",")
+            person = Person(answers[1])
+
+            #check every answer and whether its negative, positive, or open ended
+            for i in range(len(answers)):
+                if answers[i] == self.negativeQ[i]:
+                    person.addMultipleChoice(answers[i], True)
+                elif answers[i] == self.positiveQ[i]:
+                    person.addMultipleChoice(answers[i], False)
+                else:
+                    person.addOpenEnded(answers[i])
+            
+            #every line is one person, and every persons sum must be calculated
+            person.summation(self.importantQ, weight)
+            self.people.append(person)
+    
+    def peopleSort(self):
+        self.people.sort(key= lambda person: person.sum)
+        for i in self.people:
+            self.names.append(i.name)
+    
+    def findMatches(self):
+        graph = dict()
+        for i in range(len(self.names)):
+            edges = self.closestValues(self.names, i, 4)
+            graph[self.names[i]] = "sink"
+            graph[self.people[i]] = edges
+        graph["source"] = self.people
+
 
 #when similar values, n, is odd then the code returns even number of similar values still
 def closestValues(list, i, n):
@@ -91,6 +106,29 @@ def closestValues(list, i, n):
     close = list[l: i]
     close.extend(list[i+1: r+1])
     return close
+
+def FordFulkerson(graph, start, end):
+    source = graph.get(start)
+    matches = []
+    
+    while len(source) != 0:
+        n = random.randint(0, len(source) - 1) #random node to travel to from source
+        names = graph.pop(source[n]) #remove from graph of gift givers
+
+        n2 = random.randint(0, len(names) - 1) #random name to match with that person
+        while names[n2] not in graph:
+            names.remove(source[n2])
+            n2 = random.randint(0, len(names))
+
+        match = (source[n], names[n2])
+
+        source.remove(source[n]) #remove from gift givers set
+        graph.pop(names[n2]) #remove from gift recievers set
+
+        matches.append(match)
+    
+    return matches
+
 
 
 def SecretSanta(fileLocal, negative, positive, importantQ):
@@ -142,7 +180,7 @@ if __name__ == "__main__":
     similarValues = 4
     index = 0
     n = 10
-    neg = -5
+    answer = "yo"
     weight = 5
     l = [a for a in range(n)]
     person = Person("Kyle")
@@ -165,7 +203,21 @@ if __name__ == "__main__":
     assert(t == t2)
 
     #0 sum
-    person.addMultipleChoice(-5, True) #dosent matter the answer since the boolean is the only thing that affects values
-    person.addMultipleChoice(5, False)
+    person.addMultipleChoice(answer, True) #dosent matter the answer since the boolean is the only thing that affects values
+    person.addMultipleChoice(answer, False)
     person.summation([], weight)
     assert(person.sum == 0)
+
+    #positive sum weighted
+    person.multipleChoice = []
+    person.negMultipleChoice = []
+    person.addMultipleChoice(answer, False)
+    person.summation([answer], weight)
+    assert(person.sum == weight)
+
+    #negative sum weighted
+    person.multipleChoice = []
+    person.negMultipleChoice = []
+    person.addMultipleChoice(answer, True)
+    person.summation([answer], weight)
+    assert(person.sum == (0 - weight))
